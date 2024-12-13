ebe3bfd4a40f8ba1692ae7da472550dc18b351f9
Juanda <heyjuns@icloud.com>
2024-12-13 23:48:28 +0700

    add JWT authentication strategy for music database

--- src/server.js
+++ src/server.js
@@ -41,6 +41,23 @@ const init = async () => {
       plugin: Jwt,
     },
   ]);
+
+  server.auth.strategy('musicdb_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
+
   await server.register([
     {
       plugin: albums,
